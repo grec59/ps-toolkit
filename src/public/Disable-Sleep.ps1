5d940abd185162935adfef6195d2124b76b6229c
grec59 <agrocker19909@gmail.com>
2025-12-02 21:44:18 -0500

    Update Disable-Sleep function output messages

--- src/public/Disable-Sleep.ps1
+++ src/public/Disable-Sleep.ps1
@@ -1,12 +1,8 @@
 function Disable-Sleep {
-    Write-Output "Disabling Sleep When Plugged In..."
-    Start-Sleep 2
-
     powercfg /change standby-timeout-ac 0
     powercfg -setacvalueindex SCHEME_CURRENT `
         4f971e89-eebd-4455-a8de-9e59040e7347 `
         5ca83367-6e45-459f-a27b-476b1d01c936 0
 
-    Write-Output "SUCCESS: Sleep and Lid Closure action when plugged in is disabled."
-    Start-Sleep 2
+    Write-Output "SUCCESS: Sleep and Lid Closure action when plugged has been disabled."
 }
