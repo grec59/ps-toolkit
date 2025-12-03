bc91c33c5e37ade8353e7d5aea1b3986f3969ce0
grec59 <agrocker19909@gmail.com>
2025-12-02 23:13:52 -0500

    Simplified logic

--- src/public/Update-Policy.ps1
+++ src/public/Update-Policy.ps1
@@ -1,15 +1,12 @@
 function Invoke-GroupPolicy {
     try {
-        Write-Host "Running Policy Update..." -ForegroundColor Cyan
-        gpupdate /target:computer | out-null
+        Write-Output "Running Policy Update..."
+        gpupdate /target:computer | Out-Null
         Start-Sleep -Seconds 5
-        Write-Host "SUCCESS: Computer Policy update has completed." -ForegroundColor Green
-        "SUCCESS: Computer Policy update completed. Check Event Viewer for details." | Out-File -FilePath $output -Encoding utf8 -Append
+        Write-Output "SUCCESS: Computer Policy update has completed."
     }
-
     catch {
-        Write-Host "FAIL: Failed to update Computer Policy. Check Event Viewer for details." -ForegroundColor Yellow
-        "FAIL: Unable to update Computer Policy." | Out-File -FilePath $output -Encoding utf8 -Append
-        $($_.Exception.Message) | Out-File -FilePath $output -Encoding utf8 -Append
+        Write-Output "FAIL: Failed to update Computer Policy. Check Event Viewer for details."
+        Write-Output "ERROR: $($_.Exception.Message)"
     }
 }
