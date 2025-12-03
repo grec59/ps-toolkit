730fdae937e00f1878b915ac35ef2e476d58179c
grec59 <agrocker19909@gmail.com>
2025-12-02 21:41:48 -0500

    Update Disable-Sleep function output methods

--- src/public/Disable-Sleep.ps1
+++ src/public/Disable-Sleep.ps1
@@ -1,9 +1,12 @@
 function Disable-Sleep {
-    Write-Host "Disabling Sleep When Plugged In..." -ForegroundColor Cyan
+    Write-Output "Disabling Sleep When Plugged In..."
     Start-Sleep 2
+
     powercfg /change standby-timeout-ac 0
-    powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
-    Write-Host "SUCCESS: Sleep and Lid Closure action when plugged in is disabled." -ForegroundColor Green
-    "SUCCESS: Sleep and Lid Closure action when plugged in has been disabled." | Out-File -FilePath $output -Encoding utf8 -Append
+    powercfg -setacvalueindex SCHEME_CURRENT `
+        4f971e89-eebd-4455-a8de-9e59040e7347 `
+        5ca83367-6e45-459f-a27b-476b1d01c936 0
+
+    Write-Output "SUCCESS: Sleep and Lid Closure action when plugged in is disabled."
     Start-Sleep 2
 }
